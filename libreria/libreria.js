8a9e0e6128b9492a824949ecb57cc6737e088562
Virano-Simone <s.virano.3343@vallauri.edu>
2025-05-08 13:08:10 +0200

    Implementazione file necessari alla realizzazione del progetto

---
+++ libreria/libreria.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const JSONSERVER_URL = "http://localhost:3000"
+//prende URL scritto e lo inserisce nella costante
+//farà quindi una richiesta alla cartella corrente
+const APACHE_URL = ""
+// Se la url è vuota, viene assegnata  l'origine da cui è stata scaricata la pagina
+
+function inviaRichiesta(method, url, parameters = {}) {
+	let axiosOptions = {
+		"baseURL": APACHE_URL,
+		"url": url,
+		"method": method.toUpperCase(),
+		"headers": {
+			"Accept": "application/json",
+		},
+		"timeout": 5000,
+		"responseType": "json",
+	}
+	if (parameters instanceof FormData) {
+		axiosOptions.headers["Content-Type"] = 'multipart/form-data;'
+		axiosOptions["data"] = parameters     // Accept Blob
+	}
+	else if (method.toUpperCase() == "GET") {
+		axiosOptions.headers["Content-Type"] = 'application/x-www-form-urlencoded;charset=utf-8'
+		axiosOptions["params"] = parameters   // Accept plain object or URLSearchParams
+	}
+	else {
+		//axiosOptions.headers["Content-Type"] = 'application/json;charset=utf-8' 		
+		axiosOptions.headers["Content-Type"] = 'application/x-www-form-urlencoded;charset=utf-8'
+		axiosOptions["data"] = parameters
+	}
+	return axios(axiosOptions)
+}
+
+
+function errore(err) {
+	if (!err.response)
+		alert("Connection Refused or Server timeout");
+	else if (err.response.status == 200)
+		alert("Formato dei dati non corretto : " + err.response.data);
+	else
+		alert("Server Error: " + err.response.status + " - " + err.response.data)
+}
+
---
+++ server/mysqli.php
@@ -0,0 +1,71 @@
+<?php
+$conn = null;
+function openConnection()
+{
+    define("DB_NAME", "banche");
+    //in caso in cui mysql sia su un'altra macchina dobbiamo immetere l'indirizzo IP della
+    //macchina su cui è installato mysql
+    define("DB_HOST", "localhost");
+    //utente creato automaticamente da db mysql quando creiamo un db
+    //serve per collegarsi al db    
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+
+    //serve per abilitare il try catch sulle righe successive
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    try {
+        //mysqli è una libreria che permette di connettersi a mysql
+        //questo istanzia una nuova connessione al db people e la ritorniamo
+        global $conn;
+        $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        //il charset è il set di caratteri che il server deve utilizzare per comunicare con il db
+        $conn->set_charset("utf8mb4");
+        return $conn;
+    } catch (mysqli_sql_exception $err) {
+        //se non riesce a connettersi ritorna un errore 503: "errore connessione db"
+        http_response_code(503);
+        //die stampa il messaggio e termina lo script
+        //per accedere ai metodi di un oggetto si usa ->
+        die("<b>Errore connessione db </b>" . $err->getMessage());
+    }
+}
+function executeQuery($conn, $sql): mixed{
+    try {
+        //query metodo dell'oggetto conn che esegue la query. nelle chiamate get 
+        // i dati sono restituiti sotto forma di recordset php
+        $result = $conn->query($sql);
+        //in tutte le chiamatate diverse dalla GET php restituisce true se è andato
+        //tutto bene, false in caso di errore
+        if (!is_bool($result)) {
+            //fetch_all converte da recordSet php a vettore enumerativo di json 
+            //solo nel caso della chiamate GET
+            $data = $result->fetch_all(MYSQLI_ASSOC);
+        } else {
+            $data = $result;
+        }
+        return $data;
+    } catch (mysqli_sql_exception $err) {
+        //chiudo la connessione e termino lo script
+        $conn->close();
+        http_response_code(500);
+        die("<b>Errore query</b>" . $err->getMessage());
+    }
+}
+
+function checkParams($param): mixed{
+    //per accedere a una variabile globale devo scrivere "global" seguito dal nome della variabile
+    global $conn;
+    //php consegna tutti i parametri dentro $_REQUEST
+    if(isset($_REQUEST[$param]))
+    //ritorna il valore del parametro(nel primo caso il contenuto del textBox)
+        return $_REQUEST[$param];
+    else
+    {
+        //errore di parametri mancanti
+        http_response_code(400);
+        $conn->close();
+        die("Parametro $param mancante");
+    }
+
+}
+?>
